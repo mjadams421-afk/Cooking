4f6eb08538799200d4f4f7c8bab81f6c8d671de3
Mat <mjadams421@gmail.com>
2026-04-17 01:40:46 +0000

    some react edits

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';  
-import 'react' from npm;
 
 createRoot(document.getElementById('app')).render(<App />);
