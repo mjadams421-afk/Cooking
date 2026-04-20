c081a588b929cb1b73266d42503d9582601be26b
Mat <mjadams421@gmail.com>
2026-04-20 21:25:07 +0000

    Added functions and handlers

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+ //Variables for the ingredients sections
 const fishIn = document.getElementById("Fish");
 const chickenIn = document.getElementById("Chicken");
 const bakingIn = document.getElementById("Baking");
-
+//Fish event listener and function
 function displayInA() {
-  fishIn.innerHTML = 'Cooking Fish: Fish Fillet, bread crumbs, herbs, Lemon, Garlic, Olive Oil, honey mustard, Salt & Pepper';
+  fishIn.innerHTML = '"Cooking Fish": Fish Fillet, bread crumbs, herbs, Lemon, Garlic, Olive Oil, honey mustard, Salt & Pepper';
   fishIn.style.backgroundColor = 'rgba(250, 250, 220, 1)';
   fishIn.style.color = 'black';
  }
+//Chicken event listener and function
+function displayInB() {
+  chickenIn.innerHTML = '"Cooking Chicken": Chicken Breast, mushrooms, Garlic, Herbs, spaghetti, Salt & Pepper';
+  chickenIn.style.backgroundColor = 'rgba(250, 250, 220, 1)';
+  chickenIn.style.color = 'black';
+}
+//Baking event listener and function
+function displayInC() {
+  bakingIn.innerHTML = '"Baking": Flour, Sugar, Eggs, Butter, Baking Powder, corn oil, Vanilla Extract';
+  bakingIn.style.backgroundColor = 'rgba(250, 250, 220, 1)';
+  bakingIn.style.color = 'black';
+}
 
+//Event Handlers for functions
  fishIn.addEventListener('click', displayInA);
+ chickenIn.addEventListener('click', displayInB);
+ bakingIn.addEventListener('click', displayInC);
